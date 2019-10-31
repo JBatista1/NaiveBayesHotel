c2926c09471fea8907b23e070ffd930567e08d23
JBatistta1 <j.batista.damasceno@icloud.com>
2019-10-30 23:10:20 -0300

    Add function for create set with datas and tranfiorm in dictionay

--- main.py
+++ main.py
@@ -22,20 +22,36 @@ o csv, as categorias criadas foram:
 class generateDataSet:
     def __init__(self, datasetName):
         self.datasetName = datasetName
+
         xTrain, yTrain, xTest, yTest  = self.ManagerData()
 
+        dictionary = self.CreateDictionayValue(xTrain)
+        print(dictionary)
+        print(len(dictionary))
     def ManagerData(self):
-        text = pd.read_csv(self.datasetName)
+        data = pd.read_csv(self.datasetName)
+
         #Cria conjunto de dados para treino e para test
-        train, test = train_test_split(text, test_size=0.20)
+        train, test = train_test_split(data, test_size=0.20)
 
         #Cria o dado que serão utilizados para treinameto somente com as infomações necessarias no caso Review_Text
-        xTrain = train.Review_Text
-        yTrain = train.Sentiment
+        xTrain = train["Review_Text"].tolist()
+        yTrain = train["Sentiment"].tolist()
         #Cria o dado que serão utilizados para teste somente com as infomações necessarias no caso Sentiment
-        xTest = test.Review_Text
-        yTest = test.Sentiment
+        xTest = test["Review_Text"].tolist()
+        yTest = test["Sentiment"].tolist()
+
         return xTrain,yTrain,xTest,yTest
+    def CreateDictionayValue(self, arrayString):
+        size = len(arrayString) - 1
+
+        dataset = set()
+        for i in range(0,size):
+            stringSplit = arrayString[i].split()
+            dataset = dataset.union(set(stringSplit))
+        dictionary = list(dataset)
+        sorted(dictionary)
+        return dictionary
 
 
 
